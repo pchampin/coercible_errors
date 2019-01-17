7a193f583a2a83b0a5bfc9a2c434312029142bd9
Pierre-Antoine Champin <pchampin@liris.cnrs.fr>
2019-01-17 19:02:14 +0100

    moved out of the macro the types that do not depend on Error
    Also fixed a bug in the macro definition,
    and improved some comments.

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,32 @@
 #[macro_export]
 macro_rules! mergeable_errors {
+
     () => {
         mergeable_errors!(Error);
     };
     ($error: ty) => {
-        mergeable_errors!(Error, Never, OkResult, MergesWith, MergedResult);
+        mergeable_errors!($error, MergesWith, MergedResult);
     };
-    ($error: ty, $never: ident, $ok_result: ident, $merges_with: ident, $merged_result: ident) => {
-        /// An "error" type that can never happen.
-        ///
-        /// NB: once the [`never`] types reaches *stable*,
-        /// this type will be an alias for the standard type.
-        ///
-        /// [`never`]: https://doc.rust-lang.org/std/primitive.never.html
-        ///
-        #[derive(Clone, Debug)]
-        pub enum $never {}
-        impl ::std::fmt::Display for $never {
-            fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-                write!(f, "$never")
-            }
+    ($error: ty, $merges_with: ident, $merged_result: ident) => {
+        pub use mergeable_errors::{Never, OkResult};
+
+        // This conversion can never happen (since Never can have no value),
+        // but it is required for allowing $error and Never to merge with each other.
+        impl From<Never> for $error {
+            fn from(_: Never) -> $error { unreachable!() }
         }
-        impl std::error::Error for $never {}
-        // This conversion can $never happen (since $never can have no value),
-        // but it is required for using `?` with `$ok_results`s.
-        impl From<$never> for $error {
-            fn from(_: $never) -> $error { unreachable!() }
-        }
-        /// Type alias for a result that will $never fail.
-        pub type $ok_result<T> = std::result::Result<T, $never>;
 
 
-        /// A utility trait for merging two types of errors.
+        /// A trait indicating mergeability with another type.
         pub trait $merges_with<E>: Sized + std::marker::Send + std::error::Error + 'static {
             type Into: std::marker::Send + std::error::Error + 'static + From<Self> + From<E>;
         }
-        impl<T: std::marker::Send + std::error::Error + 'static> $merges_with<$error> for T where $error: From<T> { type Into = $error; }
-        impl<T: std::marker::Send + std::error::Error + 'static> $merges_with<$never> for T where T: From<$never> { type Into = T; }
+        impl<T: std::marker::Send + std::error::Error + 'static> $merges_with<$error> for T
+            where $error: From<T>
+        { type Into = $error; }
+        impl<T: std::marker::Send + std::error::Error + 'static> $merges_with<Never> for T
+            where T: From<Never>
+        { type Into = T; }
 
 
         /// A shortcut for building the merged result type,
@@ -47,3 +37,25 @@ macro_rules! mergeable_errors {
         pub type $merged_result<T, E1, E2> = std::result::Result<T, <E1 as $merges_with<E2>>::Into>;
     };
 }
+
+/// An "error" type that can never happen.
+///
+/// NB: once the [`never`] types reaches *stable*,
+/// this type will be an alias for the standard type.
+///
+/// [`never`]: https://doc.rust-lang.org/std/primitive.never.html
+///
+#[derive(Clone, Debug)]
+pub enum Never {}
+impl ::std::fmt::Display for Never {
+    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+        write!(f, "Never")
+    }
+}
+impl std::error::Error for Never {}
+
+
+/// Type alias for a result that will Never fail.
+pub type OkResult<T> = std::result::Result<T, Never>;
+
+
